de3521bed99ca820c052b90ab1e7269197665925
Zhong Wanying <1779904692@qq.com>
2024-03-15 22:59:16 +0800

    重新上传

--- 使用CukeTest进行BDD自动化测试/案例项目/二手汽车后台数据管理系统/springboot/src/main/java/com/example/demo/mapper/DynamicMapper.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.demo.entity.Dynamic;
-
-public interface DynamicMapper extends BaseMapper<Dynamic> {
-}
